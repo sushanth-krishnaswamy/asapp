17350bad9d6840668500135dc2e40e4f8a72f741
sushanth-krishnaswamy <sushanth.k4@gmail.com>
2017-05-04 16:09:45 -0400

    fixed indentation errors and minor changes

--- chat_system/server.py
+++ chat_system/server.py
@@ -1,3 +1,4 @@
+
 import os
 import select
 import socket
@@ -5,151 +6,147 @@ import sys
 import signal
 from time import sleep
 
-
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_PSS
 from Crypto.Hash import SHA
 
-from communication import send,recieve
+from communication import send, receive
+
 
 class Server_Chat(object):
-	
-	def __init__(self,address='127.0.0.1', port= 3490):
-		self.client = 0
 
-		self.clientmap = {}
+    def __init__(self, address='127.0.0.1', port=3490):
+        self.clients = 0
 
-		self.outputs = []
+    
+        self.clientmap = {}
 
-		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # This is the output socket list
+        self.outputs = []
+
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((address, int(port)))
 
-        print "Generating the RSA keys now..."
-        self.server_private_key = RSA.generate(4096, os.random)
-        self.server.bind((address,int(port)))
+        print 'Generating the RSA keys ...'
+        self.server_privkey = RSA.generate(4096, os.urandom)
+        self.server_pubkey = self.server_privkey.publickey()
 
-        print "Now, listening to port on port : ",port,"..."
+        print 'Listening to port on port: ', port, '...'
         self.server.listen(5)
 
-        #this is to trap keyboard interrupts
+        # This is to trap keyboard interrupts
         signal.signal(signal.SIGINT, self.sighandler)
 
+    def sighandler(self, signum, frame):
 
-    def sighandler(self,signum,frame):
-    	#close the server --- very important to not cause data leakage
+        # server closing
+        print 'Shutting down the server...'
 
-    	print "shutting down the server now ..."
+        # Close the existing client sockets
+        for o in self.outputs:
+            o.close()
 
-    	for i in self.outputs:
-    		i.close()
+        self.server.close()
 
-    	self.sever.close()
+    def getname(self, client):
+        # Return the name of the user(client)
 
+        info = self.clientmap[client]
+        host, name = info[0][0], info[1]
 
-    def getname(self,client):
+        return '@'.join((name, host))
 
-    	#returns the name of the user(client) 
+    def get_just_name(self, client):
+        return self.clientmap[client][1]
 
-    	info = self.clientmap[client]
-    	host,name = info[0][0], info[1]
+    def send_encrypted(self, to_who, message, name):
+        try:
+            encryptor = self.clientmap[to_who][2]
+            msg = encryptor.encrypt(message, 0)
+            send(to_who, msg)
 
-    	return '@'.join((name,host))
+        except IOError:
+            send(to_who, 'PLAIN: cannot find public key for: %s' % name)
 
-    def get_just_name(self,client):
-    	return self.clientmap[client][1]
+    def verify_signature(self, client, message, signature):
+        try:
+            key = self.clientmap[client][2]
+            msg_hash = SHA.new()
+            msg_hash.update(message)
 
-    def send_encrypted(self,to_who,message,name):
-    	try:
-    		encryptor = self.clientmap[to_who][2]
-    		msg = encryptor.encrypt(message,0)
-    		send(to_who,msg)
+            verifier = PKCS1_PSS.new(key)
+            return verifier.verify(msg_hash, signature)
 
-    	except IOError:
-    		send(to_who,'PLAIN: cannot find public key for: %s' , %name)
-
-    def verify_signature(self,client,message,signature):
-    	try:
-    		key = self.clientmap[client][2]
-    		msg_hash = SHA.new()
-    		msg_hash.update(message)
-
-    		verifier = PKCS1_PSS.new(key)
-    		return verifier.verofy(msg_hash,signature)
-
-    	except IOError:
-    		return False
+        except IOError:
+            return False
 
     def serve(self):
-    	inputs = [self.server, sys.stdin]
-    	self.outputs = []
+        inputs = [self.server, sys.stdin]
+        self.outputs = []
 
-    	running = 1
+        running = 1
 
-    	while running:
-    		try:
-    			inputready, outputready, exceptready = select.select(inputs, self.ooutputs, [])
+        while running:
+            try:
+                inputready, outputready, exceptready = select.select(inputs, self.outputs, [])
 
+            except select.error:
+                break
 
-    		except select.error:
-    			break
-    		except socket.error:
-    			break
+            except socket.error:
+                break
 
-    		for s in inputready:
-    			if s == self.server:
-    				# handle this server socket properly
+            for s in inputready:
+                if s == self.server:
+                    # handle the server socket correctly
 
-    				client, address = self.server.accept()
-    				print "The chat server got connection: connection %d is from %s", %(client.fileno(),address)
+                    client, address = self.server.accept()
+                    print 'The chatserver got connection: connection %d is from %s' % (client.fileno(), address)
+                    # Getting the  client public key and sending the server's public key
+                    pubkey = RSA.importKey(receive(client))
+                    send(client, self.server_pubkey.exportKey())
 
-    				#now, getting the client's public key and send the server's public key
+                    # get and read the login name
+                    cname = receive(client).split('NAME: ')[1]
 
-    				pubkey = RSA.importKey(recieve(client))
-    				send(client,self.server_pubkey.exportKey())
+                    #send client name back to terminal
+                    self.clients += 1
+                    send(client, 'CLIENT: ' + str(address[0]))
+                    inputs.append(client)
 
-    				#get and read the login name
-    				cname = receieve(client).split('NAME: ')[1]
+                    self.clientmap[client] = (address, cname, pubkey)
 
+                    # Send client's joining information to other people(clients)
 
-    				#sending the client name back to display
-    				self.clients += 1
-    				send(client, 'CLIENT: '+ str(address,cname,pubkey))
+                    msg = '\n(Connected: New client (%d) from %s)' % (self.clients, self.getname(client))
 
+                    for o in self.outputs:
+                        try:
+                            self.send_encrypted(o, msg, self.get_just_name(o))
 
-    				#sending this client's joining info to other people(clients)
+                        except socket.error:
+                            self.outputs.remove(o)
+                            inputs.remove(o)
 
-    				msg = '\n(Connected : New client (%d) from %s)' % (self.clients, self.getname(client))
+                    self.outputs.append(client)
 
-    				for i in self.outputs:
-    					try:
-    						self.send_encrypted(i,msg,self.get_just_name(i))
+                elif s == sys.stdin:
+                    # handle standard input
+                    sys.stdin.readline()
+                    running = 0
+                else:
 
-    					except socket.error:
-    						self.outputs.remove(i)
-    						inputs.remove(i)
+                    # handle all other sockets
+                    try:
+                        data = receive(s)
 
-    				self.outputs.append(client)
+                        if data:
+                            dataparts = data.split('#^[[')
+                            signature = dataparts[1]
+                            data = dataparts[0]
 
-    			elif s == sys.stdin:
-
-    				#handle standard input
-
-    				sys.stdin.readline()
-    				running = 0
-    			else:
-
-    				#handling all the other sockets
-    				try:
-    					data = receive(s)
-
-    					if data:
-    						dataparts = data.split('#^[[')
-    						signature = dataparts[1]
-
-    						data = dataparts[0]
-
-    						verified = self.verify_signature(s, data, signature)
+                            verified = self.verify_signature(s, data, signature)
                             data = self.server_privkey.decrypt(data)
 
                             if data != '\x00':
@@ -157,20 +154,20 @@ class Server_Chat(object):
                                     data = '%s [OK]' % data
 
                                 else:
-                                	data = '%s [Not verified]' %data
+                                    data = '%s [Not verified]' % data
 
-                               	# Send as new client's message...
+                                # Send as new client's message...
                                 msg = '\n# [' + self.getname(s) + ']>> ' + data
 
                                 # Send data to all except ourselves
 
-                                for i in self.outputs:
-                                    if i != s:
+                                for o in self.outputs:
+                                    if o != s:
                                         self.send_encrypted(o, msg, self.get_just_name(s))
 
                         else:
 
-                        	print 'Chatserver: Client %d hung up' % s.fileno()
+                            print 'Chatserver: Client %d hung up' % s.fileno()
                             self.clients -= 1
                             s.close()
                             inputs.remove(s)
@@ -179,24 +176,21 @@ class Server_Chat(object):
                             # Send client leaving information to others
                             msg = '\n(Hung up: Client from %s)' % self.getname(s)
 
-                            for i in self.outputs:
-                                self.send_encrypted(i, msg, self.get_just_name(i))
-
+                            for o in self.outputs:
+                                self.send_encrypted(o, msg, self.get_just_name(o))
 
                     except socket.error:
                         # Remove
                         inputs.remove(s)
                         self.outputs.remove(s)
 
-            
-            sleep(0.5)
+            sleep(0.1)
 
         self.server.close()
 
-if __name__ == "__main__" :
-	if len(sys.argv) < 3:
-		sys.exit('Usage: %s listening_ip listening_port' %sys.argv[0])
+if __name__ == "__main__":
 
-	Server_Chat(sys.argv[1],sys.argv[2].serve())
+    if len(sys.argv) < 3:
+        sys.exit('Usage: %s listening_ip listening_port' % sys.argv[0])
 
-	        	
+    Server_Chat(sys.argv[1], sys.argv[2]).serve()
